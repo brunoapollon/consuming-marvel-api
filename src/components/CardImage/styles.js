1d789a8e260ed87fb67a53d0c3bcff12d3263ffd
Bruno Lopes <eusoubruno7@gmail.com>
2021-09-05 11:58:43 -0300

    adjust: adjusting cardImage component

--- src/components/CardImage/styles.js
+++ src/components/CardImage/styles.js
@@ -3,11 +3,22 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100%;
+  position: relative;
   img {
     width: 200px;
     height: 100%;
     -webkit-mask-image: linear-gradient(to top, transparent 0, black 50%);
     mask-image: linear-gradient(to top, transparent 0, black 50%);
+    border-radius: 10px;
+    object-fit: cover;
+    &:hover {
+      -moz-transform: scale(1.3);
+      -webkit-transform: scale(1.3);
+      transform: scale(1.3);
+      position: relative;
+      margin: 0 30px;
+      transition-delay: 0.1s;
+    }
   }
 
   & + div {
--- src/styles/global.js
+++ src/styles/global.js
@@ -9,6 +9,18 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     height: 100vh;
+    &::-webkit-scrollbar {
+      width: 5px;
+    }
+
+    &::-webkit-scrollbar-track {
+      background: transparent;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: #ed1d24;
+      border-radius: 10px;
+    }
   }
   h1,h2,h3,h4,h5, span, input, a, button{
     font-family: 'Roboto', serif;
