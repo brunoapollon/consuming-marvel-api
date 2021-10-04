7c8c5048cc99300e30163103684cf7d6b89d1fe8
Bruno Lopes <eusoubruno7@gmail.com>
2021-10-04 14:02:28 -0300

    feat: adding responsivity page in events page and about page

--- src/components/SectionImages/styles.js
+++ src/components/SectionImages/styles.js
@@ -2,23 +2,16 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%max-content;
   overflow: hidden;
   margin-bottom: 200px;
-  box-sizing: border-box;
   h1 {
-    font-family: "Montserrat", sans-serif;
     font-size: 60px;
     color: #fff;
     margin-left: 50px;
   }
   .scroll-container {
-    box-sizing: border-box;
-    width: 100%;
-    height: 300px;
     display: flex;
-    overflow: hidden;
-    margin: 0 50px 0;
+    width: 100%;
   }
   @media (max-width: 1024px) {
     margin-bottom: 100px;
--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -6,20 +6,25 @@ export const Container = styled.div`
 `;
 export const Content = styled.div`
   height: 100%;
-  width: 1024px;
+  max-width: 1024px;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
   > div {
+    padding: 5px;
+    box-sizing: border-box;
+    width: 100%;
     > p {
       text-align: justify;
-      margin: 0;
       color: #fff;
     }
   }
   .footer {
+    width: 100%;
+    margin: 0;
+    padding: 0;
     position: absolute;
     bottom: 0;
   }
